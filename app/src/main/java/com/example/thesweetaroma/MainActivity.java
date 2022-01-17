35369f2c66fb3a68f28e1023d37181cc8d41292a
HabibaAkter562 <habiba.akter1.cse@ulab.edu.bd>
2022-01-17 22:20:30 +0600

    MainActivityAdd

--- app/src/main/java/com/example/thesweetaroma/MainActivity.java
+++ app/src/main/java/com/example/thesweetaroma/MainActivity.java
@@ -1,27 +1,101 @@
 package com.example.thesweetaroma;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-     TextView createnewAccount;
+    TextView createnewAccount;
+
+    EditText emailLog, passwordLog;
+    Button btn_login;
+    String emailPattern = "[a-zA-z0-9._-]+@[a-z]+\\.[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-   createnewAccount=findViewById(R.id.createnewAccount);
-   getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-   createnewAccount.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View v) {
-           startActivity(new Intent(MainActivity.this,RegisterActivity.class));
-       }
-   });
+        createnewAccount = findViewById(R.id.createnewAccount);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        emailLog = findViewById(R.id.emailLog);
+        passwordLog = findViewById(R.id.passwordLog);
+        btn_login = findViewById(R.id.btn_login);
 
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        createnewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                perforLogin();
+            }
+        });
+
+    }
+
+    private void perforLogin() {
+        String email = emailLog.getText().toString();
+        String password = passwordLog.getText().toString();
+
+
+        if (!email.matches(emailPattern)) {
+            emailLog.setError("Enter context email");
+        } else if (password.isEmpty() || password.length() < 6) {
+            passwordLog.setError("Enter proper password");
+        } else {
+            progressDialog.setMessage("Please wait while login");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    progressDialog.dismiss();
+                    sendUserToNextActivity();
+                    Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                }
+                }
+            });
+        }
+    }
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
