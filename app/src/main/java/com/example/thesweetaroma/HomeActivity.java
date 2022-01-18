a1f00004569a8adcf2ab49054e5bcb724a315974
HabibaAkter562 <habiba.akter1.cse@ulab.edu.bd>
2022-01-18 20:15:43 +0600

    HomeActivity

--- app/src/main/java/com/example/thesweetaroma/HomeActivity.java
+++ app/src/main/java/com/example/thesweetaroma/HomeActivity.java
@@ -2,13 +2,35 @@ package com.example.thesweetaroma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    TextView menu;
 
+    Button btn_Cookies,btn_Cupcakes,btn_Chocolatebar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        menu = findViewById(R.id.menu);
+        btn_Cookies = findViewById(R.id.btn_Cookies);
+        btn_Cupcakes = findViewById(R.id.btn_Cupcakes);
+        btn_Chocolatebar = findViewById(R.id.btn_Chocolatebar);
+
+        btn_Chocolatebar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, CustomizeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+            });
+
     }
 }
+
