ac95c23573389cfe7131f674d5832e4323106c37
HabibaAkter562 <habiba.akter1.cse@ulab.edu.bd>
2022-01-22 22:29:04 +0600

    ProfileCode

--- app/src/main/java/com/example/thesweetaroma/ProfileActivity.java
+++ app/src/main/java/com/example/thesweetaroma/ProfileActivity.java
@@ -2,13 +2,26 @@ package com.example.thesweetaroma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    Button btn_logOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        btn_logOut= findViewById(R.id.btn_logOut);
+
+        btn_logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
