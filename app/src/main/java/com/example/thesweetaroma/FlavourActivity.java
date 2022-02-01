17bad5888504a9b4a1028f8047016753c795e08c
HabibaAkter562 <habiba.akter1.cse@ulab.edu.bd>
2022-02-01 20:48:54 +0600

    new

--- app/src/main/java/com/example/thesweetaroma/FlavourActivity.java
+++ app/src/main/java/com/example/thesweetaroma/FlavourActivity.java
@@ -2,13 +2,34 @@ package com.example.thesweetaroma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FlavourActivity extends AppCompatActivity {
 
+     ImageButton btn_BackMenu;
+     Button btn_flavour;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flavour);
+
+        btn_BackMenu = findViewById(R.id.btn_BackMenu);
+        btn_flavour = findViewById(R.id.btn_flavour);
+
+        btn_BackMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FlavourActivity.this, HomeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/thesweetaroma/ShapeActivity.java
+++ app/src/main/java/com/example/thesweetaroma/ShapeActivity.java
@@ -2,13 +2,26 @@ package com.example.thesweetaroma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ShapeActivity extends AppCompatActivity {
-
+   ImageButton btn_Backshape;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shape);
+        btn_Backshape = findViewById(R.id.btn_Backshape);
+        btn_Backshape.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Intent intent = new Intent(ShapeActivity.this, FlavourActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+            }
+        });
     }
 }
