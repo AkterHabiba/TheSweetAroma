78e734b60188c14c67016ac817289fb07882c7f0
HabibaAkter562 <habiba.akter1.cse@ulab.edu.bd>
2022-02-02 20:53:33 +0600

    new

--- app/src/main/java/com/example/thesweetaroma/ProfileActivity.java
+++ app/src/main/java/com/example/thesweetaroma/ProfileActivity.java
@@ -5,9 +5,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -52,6 +55,43 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, UpdateActivity.class));
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
 
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/thesweetaroma/UpdateActivity.java
@@ -0,0 +1,56 @@
+package com.example.thesweetaroma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+
+    private EditText inputName, inputEmail, inputAddress, inputPhone;
+    private Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+
+
+        inputName = (EditText) findViewById(R.id.name);
+        inputEmail = (EditText) findViewById(R.id.email);
+
+        inputAddress = (EditText) findViewById(R.id.address);
+        inputPhone   = (EditText) findViewById(R.id.phone);
+        btnSave = (Button) findViewById(R.id.btn_save);
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("users");
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = inputName.getText().toString();
+                String email = inputEmail.getText().toString();
+                String address = inputAddress.getText().toString();
+                String phone = inputPhone.getText().toString();
+
+                createUser(name, email,address,phone);
+            }
+        });
+    }
+
+    private void createUser(String name, String email, String address, String phone) {
+        String key =mFirebaseDatabase.push().getKey();
+        User user = new User(name, email,address,phone);
+        mFirebaseDatabase.child(key).setValue(user);
+        finish();
+    }
+}
