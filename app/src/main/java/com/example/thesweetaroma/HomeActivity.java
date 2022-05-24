0eacc692a56d697cfa5bc429abeb1d11767254c4
HabibaAkter562 <69566383+HabibaAkter562@users.noreply.github.com>
2022-05-24 12:43:36 +0600

    Any

--- app/src/main/java/com/example/thesweetaroma/HomeActivity.java
+++ app/src/main/java/com/example/thesweetaroma/HomeActivity.java
@@ -13,16 +13,20 @@ import android.widget.TextView;
 public class HomeActivity extends AppCompatActivity {
     TextView menu;
 
-    Button btn_Cookies,btn_Cupcakes,btn_Chocolatebar,btn_profile;
+    Button btn_Cookies,btn_Cupcakes,btn_Macroons,btn_Donut,btn_Brownies,btn_Cakepop,btn_profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         menu = findViewById(R.id.menu);
+
         btn_Cookies = findViewById(R.id.btn_Cookies);
-        btn_Cupcakes = findViewById(R.id.btn_Cupcakes);
-        btn_Chocolatebar = findViewById(R.id.btn_Chocolatebar);
+        btn_Cupcakes = findViewById(R.id.btn_Cupcake);
+        btn_Macroons = findViewById(R.id.btn_Macroons);
+        btn_Donut=findViewById(R.id.btn_Donut);
         btn_profile = findViewById(R.id.btn_profile);
+        btn_Brownies= findViewById(R.id.btn_Brownies);
+        btn_Cakepop = findViewById(R.id.btn_Cakepop);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         btn_profile.setOnClickListener(new View.OnClickListener() {
@@ -50,15 +54,40 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        btn_Chocolatebar.setOnClickListener(new View.OnClickListener() {
+        btn_Macroons.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this, FlavourActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             }
-            });
+        });
+        btn_Donut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, FlavourActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
+        btn_Brownies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, FlavourActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        btn_Cakepop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, FlavourActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
